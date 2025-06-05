613858a8ac3a4843b3e2f2fe0e4062195f77bb64
SolarOctane <eboscan21@gmail.com>
2025-06-05 01:30:10 +0000

    Added dataset and some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
+st.title("🎈 My Mid Term Project App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is an immigration encounters app."
 )
+
+st.title("Upload and Display Dataset")
+
+# File uploader
+#uploaded_file = st.file_uploader("encounters", type="csv")
+
+#if uploaded_file is not None:
+    #df = pd.read_csv(uploaded_file)
+    #st.write("### Preview of the dataset:")
+    #st.dataframe(df)
+
+
+st.title("Load Local Dataset")
+
+# Load dataset (make sure the file is in the same directory or provide full path)
+df = pd.read_csv("encounters.csv")
+
+st.write("### Preview of the dataset:")
+st.dataframe(df)
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
